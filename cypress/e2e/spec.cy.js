84e55f304b72898a39e78254bdbbe0204461577b
Beca Alves <rebekaa128@gmail.com>
2026-04-26 16:30:32 +0100

    Adding user data json

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
+import userData from '../fixtures/user-data.json'
+
 
 describe('OrangeHRM TESTS', () => {
-
 
     const selectorList = {  
 
@@ -10,20 +11,21 @@ describe('OrangeHRM TESTS', () => {
         sectionTitle: ".oxd-topbar-header-breadcrumb> .oxd-text",
         wrongCredentialAlert: ".oxd-alert"
     }
+
     it (" Login com sucess", () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.get(selectorList.usernameField).type("Admin")
-        cy.get(selectorList.passwordField).type("admin123")
+        cy.get(selectorList.usernameField).type(userData.userSucess.username)
+        cy.get(selectorList.passwordField).type(userData.userSucess.password)
         cy.get(selectorList.loginButton).click()
-        cy.location("pathname").should("include", "/dashboard")
+        cy.location("pathname", {timeout: 10000}).should("include", "/dashboard/index")
         cy.get(selectorList.sectionTitle).contains("Dashboard")
     })
 
 
     it (" Login - fail", () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.get(selectorList.usernameField).type("test")
-        cy.get(selectorList.passwordField).type("test")
+        cy.get(selectorList.usernameField).type(userData.userFail.username)
+        cy.get(selectorList.passwordField).type(userData.userFail.password)
         cy.get(selectorList.loginButton).click()
         cy.get(selectorList.wrongCredentialAlert)
     })
